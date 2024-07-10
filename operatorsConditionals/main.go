ee368bdbaf577bd7fb4bfa80db47a7d89497a53e
Yeison Pinzón <45904028@claro.com.co>
2024-07-10 11:55:08 -0500

    Clase consicionales

--- operatorsConditionals/main.go
+++ operatorsConditionals/main.go
@@ -52,4 +52,64 @@ func main() {
 	fmt.Println(yearsOld < 25 && yearsOld == 32 || yearsOld < 40)
 	fmt.Println(yearsOld < 25 && (yearsOld == 32 || yearsOld < 40))
 
+	//condicional if
+	fmt.Println()
+	yearsOldNew := 20
+
+	if yearsOldNew > 18 {
+		fmt.Printf("%d is higher than 18\n", yearsOldNew)
+	}
+
+	boolVal := true
+
+	if boolVal {
+		fmt.Println("Is true")
+	} else {
+		fmt.Println("Is false")
+	}
+
+	if value := true; value {
+		fmt.Println("values is: ", value)
+	}
+
+	number := 3
+
+	if number == 1 {
+		fmt.Println("one")
+	} else if number == 2 {
+		fmt.Println("two")
+	} else if number == 3 {
+		fmt.Println("three")
+	}
+
+	switch number {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Println("undefined number")
+	}
+
+	switch numberNew := 1; numberNew {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Println("undefined number")
+	}
+
+	switch {
+	case number > 0:
+		fmt.Println("positive")
+	case number < 0:
+		fmt.Println("negative")
+	case number == 0:
+		fmt.Println("Zero")
+	}
 }
